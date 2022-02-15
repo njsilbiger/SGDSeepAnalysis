73ba4dcb8f954981e86d113e8356180f71af7f32
Silbiger <nyssa.silbiger@csun.edu>
2022-02-15 11:01:07 -0800

    updated PCA

--- Scripts/PCA_Script.R
+++ Scripts/PCA_Script.R
@@ -1,5 +1,5 @@
 # A script to create PCAs for 24 hour biogeochem data for Cabral and Varari
-# Edited on 1/8/2022
+# Edited on 2/15/2022
 # Created by Nyssa Silbiger 
 
 ####################################
@@ -46,18 +46,21 @@ Data<-Data %>%
 
 ### Varari #####
 ## There seems to be a contaminated nutrient sample for V2 Low tide on the 8/8/2021.  Remove this point
-remove<-Data %>% filter(CowTagID=="V2", Tide =="Low", Day_Night=="Day", Date == ymd("2021-08-08"))
+remove<-Data %>% filter(CowTagID=="V2", Tide ==" Low",Day_Night=="Day", Date == ymd("2021-08-08"))
+
+remove3<-Data %>% filter(CowTagID== "C4", Tide =="Low", Day_Night=="Night", Date == ymd("2021-08-09"))
 
 ## also filter out all the data from the first low tide where the water level was super high
 
-remove2<-Data %>% filter(CowTagID=="V2", Tide =="Low", Day_Night=="Day", Date == ymd("2021-08-08"))
+remove2<-Data %>% filter(Tide =="Low", Day_Night=="Day", Date == ymd("2021-08-06"))
 
 # extract the params for the PCA
 V_pca_Data<-Data %>%
   anti_join(remove)%>%
   anti_join(remove2)%>%
   filter(Location == "Varari", Plate_Seep=="Plate") %>%
-  select(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL ) %>%
+  #select(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL ) %>%
+  select(Salinity,pH,Phosphate_umolL:Lignin_Like )%>%
   drop_na()
 
 # Run the PCA
@@ -73,14 +76,15 @@ V_pca_Data_all<-Data %>%
   anti_join(remove)%>%
   anti_join(remove2)%>%
   filter(Location == "Varari", Plate_Seep=="Plate") %>%
-  drop_na(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL) %>%
+ # drop_na(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL) %>%
+  drop_na(Salinity,pH,Phosphate_umolL:Lignin_Like )%>%
   bind_cols(PC_scores)
 
 # scores plot
 p1<-V_pca_Data_all %>%
   ggplot(aes(x = PC1, y = PC2, color = Tide, shape = Day_Night))+
   geom_point() +
-  coord_cartesian(xlim = c(-4, 7), ylim = c(-4, 4)) +
+  coord_cartesian(xlim = c(-4, 7), ylim = c(-6, 4)) +
   scale_shape_manual(values = c(22,16))+
   scale_colour_hue(l = 45)+
   scale_fill_hue(l = 45)+
@@ -94,31 +98,16 @@ p1<-V_pca_Data_all %>%
 # loadings plot 
 p2<-PC_loadings %>%
   ggplot(aes(x=PC1, y=PC1, label=labels))+
-    geom_segment(aes(x=0,y=0,xend=PC1*5,yend=PC2*5),
+    geom_segment(aes(x=0,y=0,xend=PC1*10,yend=PC2*10),
       arrow=arrow(length=unit(0.1,"cm")), color = "grey")+
-  annotate("text", x = PC_loadings$PC1*5+0.1, y = PC_loadings$PC2*5+.1,
+  annotate("text", x = PC_loadings$PC1*10+0.1, y = PC_loadings$PC2*10+.1,
            label = PC_loadings$labels)+
-  coord_cartesian(xlim = c(-4, 8), ylim = c(-4, 4)) +
+  coord_cartesian(xlim = c(-4, 8), ylim = c(-6, 4)) +
    theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-# loadings plot 
-p2<-PC_loadings %>%
-  ggplot(aes(x=PC1, y=PC1, label=labels))+
-  geom_segment(aes(x=0,y=0,xend=PC1*5,yend=PC2*5),
-               arrow=arrow(length=unit(0.1,"cm")), color = "grey")+
-  annotate("text", x = PC_loadings$PC1*5+0.1, y = PC_loadings$PC2*5+.1,
-           label = PC_loadings$labels)+
-  coord_cartesian(xlim = c(-4, 8), ylim = c(-4, 4)) +
-  theme_bw()+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-
-p1+p2+ 
-  patchwork::plot_annotation("Varari", 
-                             theme = theme(plot.title = element_text(size = rel(1.5), face = "bold", hjust = 0.5, 
-                                                                     margin = margin(t = 10, b = 20, unit = "pt"))))
 VarariPCA<-p1+p2+ 
-  patchwork::plot_annotation("Varari", 
+  patchwork::plot_annotation("Varari Plates", 
                              theme = theme(plot.title = element_text(size = rel(1.5), face = "bold", hjust = 0.5, 
                                                                      margin = margin(t = 10, b = 20, unit = "pt"))))
 
@@ -127,8 +116,10 @@ ggsave(plot = VarariPCA, filename = here("Output","VarariPCA.pdf"), width = 12, 
 
 #Extract the cabral data
 C_pca_Data<-Data %>%
+  anti_join(remove3)%>%
   filter(Location == "Cabral", Plate_Seep=="Plate") %>%
-  select(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL ) %>%
+  select(Salinity,pH,Phosphate_umolL:Lignin_Like )%>%
+  #select(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL ) %>%
   drop_na()
 
 # Run the PCA
@@ -141,9 +132,10 @@ PC_loadingsC<-as_tibble(pca_C$rotation) %>%
 
 # Put it with all the original data
 C_pca_Data_all<-Data %>%
+  anti_join(remove3)%>%
   select(!Jamie_Plate_ID)%>% # Jamie's plates are all NA here
   filter(Location == "Cabral", Plate_Seep=="Plate") %>%
-  drop_na(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL) %>%
+  drop_na(Salinity,pH,Phosphate_umolL:Lignin_Like) %>%
   bind_cols(PC_scoresC)  
 
 # scores plot
@@ -164,16 +156,16 @@ p1c<-C_pca_Data_all %>%
 # loadings plot 
 p2c<-PC_loadingsC %>%
   ggplot(aes(x=PC1, y=PC1, label=labels))+
-  geom_segment(aes(x=0,y=0,xend=PC1*5,yend=PC2*5),
+  geom_segment(aes(x=0,y=0,xend=PC1*10,yend=PC2*10),
                arrow=arrow(length=unit(0.1,"cm")), color = "grey")+
-  annotate("text", x = PC_loadingsC$PC1*5+0.1, y = PC_loadingsC$PC2*5+.1,
+  annotate("text", x = PC_loadingsC$PC1*10+0.1, y = PC_loadingsC$PC2*10+.1,
            label = PC_loadingsC$labels)+
   coord_cartesian(xlim = c(-6, 6), ylim = c(-6, 6)) +
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 CabralPCA<-p1c+p2c+ 
-  patchwork::plot_annotation("Cabral", 
+  patchwork::plot_annotation("Cabral Plates", 
                              theme = theme(plot.title = element_text(size = rel(1.5), face = "bold", hjust = 0.5, 
                                                                      margin = margin(t = 10, b = 20, unit = "pt"))))
 
@@ -183,7 +175,7 @@ ggsave(plot = CabralPCA, filename = here("Output","CabralPCA.pdf"), width = 12, 
 #Varari
 V_pca_Seep<-Data %>%
   filter(Location == "Varari", Plate_Seep=="Seep") %>%
-  select(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL ) %>%
+  select(Salinity,pH,Phosphate_umolL:Lignin_Like ) %>%
   drop_na()
 
 # Run the PCA
@@ -197,23 +189,25 @@ PC_loadings_Seep<-as_tibble(pca_V_Seep$rotation) %>%
 # Put it with all the original data
 V_pca_Data_all_Seep<-Data %>%
   filter(Location == "Varari", Plate_Seep=="Seep") %>%
-  drop_na(Silicate_umolL) %>%
+  drop_na(Salinity,pH,Phosphate_umolL:Lignin_Like ) %>%
   bind_cols(PC_scores_Seep)
 
 # scores plot
 p1seep<-V_pca_Data_all_Seep %>%
   drop_na(Depth)%>%
-  ggplot(aes(x = PC1, y = PC2, color = Depth))+
-   geom_point(aes(size = Depth)) +
+  ggplot(aes(x = PC1, y = PC2, color = -Depth))+
+   geom_point(aes(size = -Depth, shape = Day_Night)) +
   #  coord_cartesian(xlim = c(-4, 7), ylim = c(-4, 4)) +
-  scale_shape_manual(values = c(22,16))+
-  scale_colour_viridis(trans = "reverse")+
-  scale_fill_viridis(trans = "reverse")+
-  geom_segment(data = PC_loadings_Seep, aes(x=0,y=0,xend=PC1*3,yend=PC2*3),
+  #scale_shape_manual(values = c(22,16))+
+  scale_colour_viridis(limits = c(-1.1,-0.4))+
+   geom_segment(data = PC_loadings_Seep, aes(x=0,y=0,xend=PC1*3,yend=PC2*3),
                arrow=arrow(length=unit(0.1,"cm")), color = "grey")+
   annotate("text", x = PC_loadings_Seep$PC1*3+0.1, y = PC_loadings_Seep$PC2*3+.1,
            label = PC_loadings_Seep$labels)+
-  scale_size(trans = "reverse")+
+  scale_size(limits = c(-1.1,-0.4))+
+  guides(color=guide_legend(), size = guide_legend())+
+  labs(color = "Water Depth", size = "Water Depth", shape = "Day or Night",
+       title = "Varari Seep")+
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
@@ -221,7 +215,7 @@ p1seep<-V_pca_Data_all_Seep %>%
 ## Cabral Seep
 C_pca_Seep<-Data %>%
   filter(Location == "Cabral", Plate_Seep=="Seep") %>%
-  select(Salinity,pH,Phosphate_umolL, Silicate_umolL, NN_umolL, Ammonia_umolL ) %>%
+  select(Salinity,pH,Phosphate_umolL:Lignin_Like ) %>%
   drop_na()
 
 # Run the PCA
@@ -235,25 +229,37 @@ PC_loadings_SeepC<-as_tibble(pca_C_Seep$rotation) %>%
 # Put it with all the original data
 C_pca_Data_all_Seep<-Data %>%
   filter(Location == "Cabral", Plate_Seep=="Seep") %>%
-  drop_na(Silicate_umolL) %>%
+  drop_na(Salinity,pH,Phosphate_umolL:Lignin_Like) %>%
   bind_cols(PC_scores_SeepC)
 
 # scores plot
 p2seep<-C_pca_Data_all_Seep %>%
   drop_na(Depth)%>%
-  ggplot(aes(x = PC1, y = PC2, color = Depth))+
-  geom_point(aes(size = Depth)) +
+  ggplot(aes(x = PC1, y = PC2, color = -Depth))+
+  geom_point(aes(size = -Depth, shape = Day_Night)) +
   #  coord_cartesian(xlim = c(-4, 7), ylim = c(-4, 4)) +
-  scale_shape_manual(values = c(22,16))+
-  scale_colour_viridis(trans = "reverse")+
-  scale_fill_viridis(trans = "reverse")+
-  geom_segment(data = PC_loadings_SeepC, aes(x=0,y=0,xend=PC1*3,yend=PC2*3),
+  #scale_shape_manual(values = c(22,16))+
+  scale_colour_viridis(limits = c(-.55,-.05))+
+#  scale_fill_viridis(trans = "reverse")+
+  geom_segment(data = PC_loadings_SeepC, aes(x=0,y=0,xend=PC1*10,yend=PC2*10),
                arrow=arrow(length=unit(0.1,"cm")), color = "grey")+
-  annotate("text", x = PC_loadings_SeepC$PC1*3+0.1, y = PC_loadings_SeepC$PC2*3+.1,
+  annotate("text", x = PC_loadings_SeepC$PC1*10+0.1, y = PC_loadings_SeepC$PC2*10+.1,
            label = PC_loadings_SeepC$labels)+
-  scale_size(trans = "reverse")+
+  scale_size(limits = c(-.55,-0.05)) +
+  guides(color=guide_legend(), size = guide_legend())+
+  labs(color = "Water Depth", size = "Water Depth", shape = "Day or Night",
+       title = "Cabral Seep")+
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+# save the plots
+SeepPCA<-p1seep+p2seep+ 
+  patchwork::plot_annotation("Seep", 
+                             theme = theme(plot.title = element_text(size = rel(1.5), face = "bold", hjust = 0.5, 
+                                                                     margin = margin(t = 10, b = 20, unit = "pt"))))
+
+ggsave(plot = SeepPCA, filename = here("Output","SeepPCA.pdf"), width = 12, height = 6)
+
 
 
 ### plots of each parameter versus depth
