e83cb324037ecf6b8ec8d7b5d20a77bd6deaae14
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2023-08-21 19:29:45 -0700

    updated some temp figures

--- Scripts/LaSourceScript.R
+++ Scripts/LaSourceScript.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(here)
 library(ggrepel)
 library(lubridate)
+library(patchwork)
 
 
 ## read in the data ###
@@ -17,7 +18,7 @@ Nordhoff<-read_csv(here("Data","Tahiti","Calibrated_CT_322_Nordhoff_20230625.csv
 AllData <-
   bind_rows(LaSource, Nordhoff)
 
-AllData %>%
+p1<-AllData %>%
   mutate(label = if_else(Date == max(Date), as.character(Site), NA_character_)) %>% 
   ggplot(aes(x = Date, y = Salinity_psu, color = Site))+
   geom_line(linewidth = 1.2)+
@@ -28,4 +29,43 @@ AllData %>%
   guides(color = FALSE)+
 theme_bw()+
   theme(text = element_text(size = 14))
+
+
+
+# Now with temperature
+p2<-AllData %>%
+  mutate(label = if_else(Date == max(Date), as.character(Site), NA_character_)) %>% 
+  ggplot(aes(x = Date, y = TempInSitu, color = Site))+
+  geom_line(linewidth = 1.2)+
+  scale_color_manual(values = c("#244474","#907a71"))+
+  labs(y = expression("Temperature " ( degree*C)),
+       x = "")+
+  geom_text(data = AllData %>% filter(Date == last(Date)), aes(label = Site, x = Date + minutes(80), y = TempInSitu, color = Site), size = 4) +
+  guides(color = FALSE)+
+  theme_bw()+
+  theme(text = element_text(size = 14))
+  
+#ggsave(here("Output","LaSource_temp.png"), width = 10, height = 6)
+
+
+p1/p2
 ggsave(here("Output","LaSource.png"), width = 10, height = 6)
+
+## plot relationship between Temp and Salinity
+p3<-AllData %>%
+ ggplot(aes(y  = TempInSitu, x = Salinity_psu, color = Site))+
+  geom_point(alpha = 0.2)+
+  annotate("text",x = 30, y = 27, label = "La Source", color = "#244474", size = 5)+
+  annotate("text",x = 34, y = 28.35, label = "Nordhoff", color = "#907a71",
+           size = 5)+
+  scale_color_manual(values = c("#244474","#907a71"))+
+  geom_smooth(method = "lm", formula = "y~poly(x,2)")+
+  labs(x = "Salinity (psu)",
+       y = expression("Temperature " ( degree*C)))+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 14))
+
+p1/p2|p3
+ggsave(here("Output","LaSource_all.png"), width = 14, height = 6)
